c967ff46645662ac4bec64e39bf70bd79f3b0c01
Williams Ozuem <ozuemdw@gmail.com>
2021-03-01 23:08:41 +0100

    commited directly from ozfood folder

---
+++ src/components/MenuList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Menu from './Menu';
+import {ProductConsumer} from '../context';
+import Title from './Title';
+export default class MenuList extends Component {
+ 
+    render() {
+        return (
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="OzFood" title="Menu" />
+                        <div className="row">
+                            <ProductConsumer>
+                                {value=>{
+                                    return value.products.map(product =>{
+                                        return <Menu key={product.id} product={product} />
+                                    })
+                                }}
+                            </ProductConsumer>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
+import styled from 'styled-components';
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                <Link to='/'>
+                  <i class="fas fa-store navbar-brand"></i>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Menu
+                        </Link>
+                    </li>              
+                </ul>
+
+                <Link to="/cart" className="ml-auto">
+                        <ButtonContainer>
+                            <span className="mr-2">
+                                <i className="fas fa-cart-plus" />
+                            </span>
+                            my cart 
+                        </ButtonContainer>
+                    </Link>  
+            </NavWrapper>
+        )
+    }
+}
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+}
+`
+
